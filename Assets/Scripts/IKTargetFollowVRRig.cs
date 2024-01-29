3bc708c8e3cf8f7e32227e1be63f8e60f6f40b0c
zheningz <zhuzhening1111@sina.com>
2024-01-29 17:00:48 +0000

    feat: upload testing features

---
+++ Assets/Scripts/IKTargetFollowVRRig.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class VRMap
+{
+    public Transform vrTarget;
+    public Transform ikTarget;
+    public Vector3 trackingPositionOffset;
+    public Vector3 trackingRotationOffset;
+
+    public void Map()
+    {
+        ikTarget.position = vrTarget.TransformPoint(trackingPositionOffset);
+        ikTarget.rotation = vrTarget.rotation * Quaternion.Euler(trackingRotationOffset);
+    }
+}
+
+public class IKTargetFollowVRRig : MonoBehaviour
+{
+    [Range(0, 1)]
+    public float turnSmoothness = 0.1f;
+    public VRMap head;
+    public VRMap leftHand;
+    public VRMap rightHand;
+
+    public Vector3 headBodyPositionOffset;
+    public float headBodyYawOffset;
+
+    private void LateUpdate()
+    {
+        // move the body according to headset's position and rotation
+        transform.position = head.ikTarget.position + headBodyPositionOffset;
+        float yaw = head.vrTarget.eulerAngles.y;
+        transform.rotation = Quaternion.Lerp(
+            transform.rotation, 
+            Quaternion.Euler(transform.eulerAngles.x, yaw, transform.eulerAngles.z), 
+            turnSmoothness
+        );
+
+        // update IK targets
+        head.Map();
+        leftHand.Map();
+        rightHand.Map();
+    }
+}
---
+++ Assets/Visual Effect/ScannerShader.shader
@@ -0,0 +1,51 @@
+﻿﻿Shader "Custom/Scanner"
+{
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+
+		ZWrite On
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float depth : DEPTH;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.depth = -(mul(UNITY_MATRIX_MV, v.vertex).z) * _ProjectionParams.w;
+				return o;
+			}
+
+			// float4 _Color_1;
+			float _Temp;
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float invert = 1 - i.depth;
+
+				if (abs(i.depth - _Temp / 2) < 0.005)
+					return fixed4(1, 1, 1, 1);
+				else
+					return fixed4(0, 0, 0,1);
+			}
+			ENDCG
+		}
+	}
+}
