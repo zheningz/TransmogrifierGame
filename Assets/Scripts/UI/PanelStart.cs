87ef3097bb9efd693d36d33ccb2d03ee7f803972
zheningz <zhuzhening1111@sina.com>
2024-03-28 14:40:45 +0000

    fix: change build settings & debug

--- Assets/Scripts/UI/PanelStart.cs
+++ Assets/Scripts/UI/PanelStart.cs
@@ -6,17 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class PanelStart : MonoBehaviour
 {
-    public GameObject items;
 
     void Start()
     {
-        items.SetActive(false);
 
         transform.Find("StartButton").GetComponent<Button>()
             .onClick.AddListener(() =>
             {
-                items.SetActive(true);
-                gameObject.SetActive(false);
+                SceneManager.LoadScene("Spider-Dungeon");
             });
 
         transform.Find("TrainButton").GetComponent<Button>()
